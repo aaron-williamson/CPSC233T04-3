abc4b8c27b5d63c32898d2d34124599efc1c1fc0
alextrapolate <chenac@ucalgary.ca>
2014-10-20 11:40:25 -0600

    updated ents
    Encapsulated and added javadoc commenting to Ents.java

--- src/Ents.java
+++ src/Ents.java
@@ -3,15 +3,22 @@ import java.util.List;
 import java.util.ArrayList;
 //this is mostly entity utility functions
 public class Ents{
-	public static List<Entity> entitylist=new ArrayList<Entity>();
+	private static List<Entity> entitylist=new ArrayList<Entity>();
 	
-	//get an array of all entities
+	/**
+	 * returns an array of all entities in the ent manager.
+	 * @return Entity[] containing all ents
+	 */
 	public static Entity[] getAll(){
 		Entity[] entsarray=new Entity[1];
 		return entitylist.toArray(entsarray);
 	}
 	
-	//this function returns the amount of entities with a given classid
+	/**
+	 * Returns the number of entities of a given classid
+	 * @param classid classid you want to count
+	 * @return number of ents
+	 */
 	public static int countByClass(String classid){
 		int n=0;
 		int size=entitylist.size();
@@ -24,7 +31,11 @@ public class Ents{
 		return n;
 	}
 	
-	//this function returns all entities of a specified class id
+	/**
+	 * Returns all of the entities of a given classid
+	 * @param classid classid of entities you want
+	 * @return Entity[] containing entities of class classid
+	 */
 	public static Entity[] getByClass(String classid){
 		Entity[] entsarray=new Entity[countByClass(classid)];
 		int size=entitylist.size();
@@ -38,12 +49,20 @@ public class Ents{
 		return entsarray;
 	}
 	
-	//add an entity to the entity array
+	/**
+	 * Add and entity to the entity manager, do this for any entity you make.
+	 * @param ent entity to add
+	 */
 	public static void addEnt(Entity ent){
 		entitylist.add(ent); 
 	}
 	
-	//returns true if there is any entity in pos (x,y)
+	/**
+	 * Returns true if there is an entity in the position, false otherwise
+	 * @param x x pos
+	 * @param y y pos
+	 * @return boolean
+	 */
 	public static boolean isEntInPos(int x,int y){
 		Entity[] entsarray=Ents.getAll();
 		for(int i=0;i<entsarray.length;i++){
@@ -54,8 +73,12 @@ public class Ents{
 		return false;
 	}
 	
-	//get Entity in pos (x,y)
-	//can use getEntInPos(x,y)!=null instead of isEntInPos(x,y)
+	/**
+	 * Gets the entity in pos x,y. If no entity in pos, returns null
+	 * @param x x pos
+	 * @param y y pos
+	 * @return Entity or null
+	 */
 	public static Entity getEntInPos(int x,int y){
 		Entity[] entsarray=Ents.getAll();
 		for(int i=0;i<entsarray.length;i++){
@@ -66,7 +89,9 @@ public class Ents{
 		return null;
 	}
 	
-	//calls think() on all entities
+	/**
+	 * Calls Entity.think() for all entities in the entity manager.
+	 */
 	public static void allThink(){
 		Entity[] entsarray=Ents.getAll();
 		for(int i=0;i<entsarray.length;i++){
